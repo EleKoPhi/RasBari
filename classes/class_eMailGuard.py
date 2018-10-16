55b2cc70f45baa892a3beb4793b7666fae7e5760
Philipp Mochti <philipp.mochti@outlook.de>
2018-10-16 23:52:54 +0800

    EmailGuad - some basic email send functions

--- classes/class_eMailGuard.py
+++ classes/class_eMailGuard.py
@@ -1,6 +1,8 @@
 import easyimap
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 from TxTMethoden import *
+import smtplib
+
 
 class eMailGuard(QObject):
 
@@ -21,13 +23,18 @@ class eMailGuard(QObject):
         self.imapper.unseen()
         self.lastMessageID = self.getLastMessagelID()
 
+        self.S = smtplib.SMTP('smtp.gmail.com', 587)
+        self.S.starttls()
+        self.S.login(self.login,self.password)
+
         print("eMailGuard - Ini - Done")
 
     def gotNewOrder(self):
         if self.lastMessageID != self.getLastMessagelID():
             self.lastMessageID = self.getLastMessagelID()
-            self.ReceiveFlag = 1
             self.CheckMail.emit()
+
+
 
     def getLastMessagelID(self):
         for mail_id in self.imapper.listids(limit=1):
@@ -38,6 +45,13 @@ class eMailGuard(QObject):
         for mail_id in self.imapper.listids(limit=1):
             mail = self.imapper.mail(mail_id)
             return mail.title
+
+    def SendIT(self):
+        message = "Message_you_need_to_send"
+        self.S.sendmail("Bar.Rasbari@gmail.com","Philipp.mochti@outlook.de",message)
+
+
+
 
 
 
