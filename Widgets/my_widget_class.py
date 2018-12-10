baf5caf80980771d2e781cd07137148c0ef24651
Philipp Mochti <philipp.mochti@outlook.de>
2018-12-10 18:23:28 +0800

    button color now shows if all ingredients for a drink are available

--- Widgets/my_widget_class.py
+++ Widgets/my_widget_class.py
@@ -37,7 +37,7 @@ class my_widget(QWidget, QObject):
 
         self.title.setGeometry(QtCore.QRect(title_x, title_y, title_width, title_height))
         self.title.setFont(title_font)
-        self.title.setText("Rasbari V9")
+        self.title.setText("Rasbari V10")
         self.stdLabelSetUp(self.title)
 
     def stdLabelSetUp(self, label):
